4032f75aea3d467813980e386a8a9671d8232a84
Pedro França <pfrutopro@gmail.com>
2023-03-17 15:22:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function stars(){
+    let count = 20;
+    let scene = document.querySelector('.scene');
+    let i =0;
+    while(i < count){
+        let star = document.createElement('i');
+        let x = Math.floor(Math.random() * window.innerWidth);
+        let duration = Math.random() * 1;
+        let h = Math.random() * 100;
+
+        star.style.left = x + 'px';
+        star.style.width = 1 + 'px';
+        star.style.height = 60 + 'px';
+        star.style.animationDuration = duration + 's';
+        
+        scene.appendChild(star);
+        i++;
+    }
+}
+stars();
